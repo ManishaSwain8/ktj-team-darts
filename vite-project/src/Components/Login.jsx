cfc010b57ca2196b3b4b91a8bcb4669b5a271019
Manisha Swain <manishaswain2003@gmail.com>
2023-01-21 19:44:48 +0530

    "new login"

--- vite-project/src/Components/Login.jsx
+++ vite-project/src/Components/Login.jsx
@@ -1,107 +1,53 @@
 import React from "react";
-
-export function Login() {
+import red from "../assets/red.jpg";
+export default function Login() {
   return (
-    <div>
-      <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              class="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-            Flowbite
-          </a>
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required=""
-                  />
-                </div>
-                <div>
-                  <label
-                    for="password"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
-                </div>
-                <div class="flex items-center justify-between">
-                  <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div class="ml-3 text-sm">
-                      <label
-                        for="remember"
-                        class="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <a
-                    href="#"
-                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-                <button
-                  type="submit"
-                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Sign in
-                </button>
-                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <a
-                    href="#"
-                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </a>
-                </p>
-              </form>
-            </div>
-          </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block">
+        <div className="justify-center">
+          <h1 className="text-white -mt-5 text-center">
+            Connect with your team.
+          </h1>{" "}
         </div>
-      </section>
-      ;
+        <img className="w-full h-full object-cover " src={red} alt="" />
+      </div>
+
+      <div className="bg-white flex flex-col justify-center">
+        <form className="max-w-[400px] w-full mx-auto bg-white p-4">
+          <h2 className="text-6xl font-bold text-center py-6">Welcome back!</h2>
+          <div className="text-xl flex-flex-col py-2">
+            <label>Username</label>
+            <input className="border rounded-full p-3 ml-1 mx-56" type="text" />
+          </div>
+          <div className="text-xl flex-flex-col py-2 gap-2 ">
+            <label>Password</label>
+            <input
+              className="border rounded-full p-3 ml-1 mx-56"
+              type="password"
+            />
+          </div>
+
+          <div className="flex justify-between">
+            <p className="flex items-center mr-2">
+              <input className="mr-2" type="checkbox" />
+              Remember me
+            </p>
+            <p>Forgot Password?</p>
+          </div>
+          <button className="border w-full my-5 py-3 bg-[#BF3737] hover:bg-[#c91d1d] text-white rounded-full">
+            Login
+          </button>
+          <br />
+          <br />
+          <div className="items-center">
+            <p>Or login with</p>
+            <br />
+            <br />
+
+            <p>Don't have an account? Register.</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
